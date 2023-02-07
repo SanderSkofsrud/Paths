d617404893f80c3a7bf2b757e0dd441dbc88a00e
Helle Rosenlind <helros@hotmail.no>
2023-02-07 12:02:38 +0100

    filled inn story class v1.0

--- src/main/java/edu/ntnu/idatt2001/Story.java
+++ src/main/java/edu/ntnu/idatt2001/Story.java
@@ -1,5 +1,43 @@
 package edu.ntnu.idatt2001;
 
+import java.util.Map;
+
 public class Story {
 
+  private String title;
+  private Map<Link, Passage> passages;
+  Passage openingPassage;
+
+  public Story(String title, Passage openingPassage) {
+    this.title = title;
+    this.openingPassage = openingPassage;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public Map<Link, Passage> getPassages() {
+    return passages;
+  }
+
+  public Passage getOpeningPassage() {
+    return openingPassage;
+  }
+
+  public void addPassage(Passage passage){
+  }
+
+  public Passage getPassage(Link link){
+    return passages.get(link);
+  }
+
+  @Override
+  public String toString() {
+    return "Story{" +
+        "title='" + getTitle() + '\'' +
+        ", passages=" + getPassages() +
+        ", openingPassage=" + getOpeningPassage() +
+        '}';
+  }
 }
