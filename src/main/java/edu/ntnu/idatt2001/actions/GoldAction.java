a84aefd6f17e1b19d4c5476c49dc8ca87948da3b
sande <sander@skofsrud.net>
2023-02-07 12:17:30 +0100

    Created Packages goals and actions, and the files within them

---
+++ src/main/java/edu/ntnu/idatt2001/actions/GoldAction.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2001.actions;
+
+import edu.ntnu.idatt2001.Player;
+
+public class GoldAction implements Action {
+  private final int gold;
+  public GoldAction(int gold) {
+    this.gold = gold;
+  }
+
+  @Override
+  public void execute(Player player) {player.addGold(this.gold);}
+}
---
+++ src/main/java/edu/ntnu/idatt2001/goals/Goal.java
@@ -0,0 +1,8 @@
+package edu.ntnu.idatt2001.goals;
+
+import edu.ntnu.idatt2001.Player;
+
+public interface Goal {
+
+  boolean isFulfilled(Player player);
+}
