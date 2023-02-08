ef4c564343cb92f6f42a145f4d7269d0a703a680
sande <sander@skofsrud.net>
2023-02-08 08:13:39 +0100

    Added tests for Passage.java

--- src/test/java/edu/ntnu/idatt2001/MainClassesTest.java
+++ src/test/java/edu/ntnu/idatt2001/MainClassesTest.java
@@ -49,5 +49,36 @@ public class MainClassesTest {
       }
     }
 
+    @Nested
+    class PassageTest {
+
+      Passage p = new Passage("Test", "Test");
+      Link link = new Link("Test1", "Test1");
+      Link link2 = new Link("Test2", "Test2");
+      List<Link> links = new ArrayList<>();
+
+
+      @Test
+      void getTitle() {
+        assertEquals("Test", p.getTitle());
+      }
+
+      @Test
+      void getContent() {
+        assertEquals("Test", p.getContent());
+      }
+
+      @Test
+      void addLink() {
+        p.addLink(link);
+        assertNotNull(p.getLinks());
+      }
+      @Test
+      void getLinks() {
+        links.add(link);
+        links.add(link2);
+        assertFalse(links.isEmpty());
+      }
+    }
 
 }
