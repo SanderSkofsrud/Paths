8baf80756d7bca1d18bfe2a23e62fd38dcd8f268
Helle Rosenlind <helros@hotmail.no>
2023-02-08 10:36:40 +0100

    added PlayerTest as a nested test in MainClassestest test-class

--- src/test/java/edu/ntnu/idatt2001/MainClassesTest.java
+++ src/test/java/edu/ntnu/idatt2001/MainClassesTest.java
@@ -81,4 +81,63 @@ public class MainClassesTest {
       }
     }
 
+    @Nested
+    class PlayerTest {
+
+      Player player = new Player("Test", 10, 30, 50);
+      List<String> inventory = new ArrayList<>();
+      String test = "Item";
+
+      @Test
+      void getName() {
+        assertEquals(player.getName(), "Test");
+      }
+
+      @Test
+      void addHealth() {
+        player.addHealth(10);
+        assertEquals(player.getHealth(),20);
+
+      }
+
+      @Test
+      void getHealth() {
+        assertEquals(player.getHealth(), 10);
+      }
+
+      @Test
+      void addScore() {
+        player.addScore(10);
+        assertEquals(player.getScore(),40);
+      }
+
+      @Test
+      void getScore() {
+        assertEquals(player.getScore(), 30);
+      }
+
+      @Test
+      void addGold() {
+        player.addGold(10);
+        assertEquals(player.getGold(),60);
+      }
+
+      @Test
+      void getGold() {
+        assertEquals(player.getGold(), 50);
+      }
+
+      @Test
+      void addToInventory() {
+        player.addToInventory(test);
+        assertNotNull(player.getInventory());
+      }
+
+      @Test
+      void getInventory() {
+        inventory.add(test);
+        assertFalse(inventory.isEmpty());
+      }
+    }
+
 }
