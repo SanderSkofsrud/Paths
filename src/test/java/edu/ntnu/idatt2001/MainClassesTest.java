939d347774767d369f8b2bc835ee0e3f245dfc30
Helle Rosenlind <helros@hotmail.no>
2023-02-08 10:44:21 +0100

    moved GameTest as a nested class to MainClassesTest test-class

--- src/test/java/edu/ntnu/idatt2001/MainClassesTest.java
+++ src/test/java/edu/ntnu/idatt2001/MainClassesTest.java
@@ -139,6 +139,30 @@ public class MainClassesTest {
         assertFalse(inventory.isEmpty());
       }
     }
+    @Nested
+    class GameTest {
+
+      @org.junit.jupiter.api.Test
+      void getPlayer() {
+      }
+
+      @org.junit.jupiter.api.Test
+      void getStory() {
+      }
+
+      @org.junit.jupiter.api.Test
+      void getGoals() {
+      }
+
+      @org.junit.jupiter.api.Test
+      void begin() {
+      }
+
+      @org.junit.jupiter.api.Test
+      void go() {
+      }
+    }
+
 
     @Nested
     class StoryTest {
