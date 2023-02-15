2d6132f7b8b1d3c0c013b019de5c23ba41823e37
Helle Rosenlind <helros@hotmail.no>
2023-02-15 14:52:39 +0100

    temporary finished player test class

--- src/test/java/edu/ntnu/idatt2001/PlayerTest.java
+++ src/test/java/edu/ntnu/idatt2001/PlayerTest.java
@@ -8,59 +8,72 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
+
   Player player = new Player("Test", 10, 30, 50);
   List<String> inventory = new ArrayList<>();
   String test = "Item";
 
   @Test
-  void getName() {
+  void testThatConstructorThrowsNullPointerException() {
+    int health = -1;
+    assertThrows(IllegalArgumentException.class, () -> new Player("name", health, 10,20));
+  }
+
+  @Test
+  void testThatConstructorThrowsIllegalArgumentException() {
+    String name = null;
+    assertThrows(NullPointerException.class, () -> new Player(name, 10,10,10));
+  }
+
+  @Test
+  void testThatAddHealthCanNotSetNegativeValue() {
+    assertThrows(IllegalArgumentException.class, () -> player.addHealth(-20));
+  }
+
+  @Test
+  void testThatItemsAddToInventory() {
+    player.addToInventory("item");
+    assertEquals(1, player.getInventory().size());
+  }
+
+  @Test
+  void testThatGetNameReturnsCorrect() {
     assertEquals(player.getName(), "Test");
   }
 
   @Test
-  void addHealth() {
+  void testThatAddHealthReturnsCorrect() {
     player.addHealth(10);
     assertEquals(player.getHealth(),20);
 
   }
 
   @Test
-  void getHealth() {
+  void testThatGetHealthReturnsCorrect() {
     assertEquals(player.getHealth(), 10);
   }
 
   @Test
-  void addScore() {
+  void testThatAddScoreReturnsCorrect() {
     player.addScore(10);
     assertEquals(player.getScore(),40);
   }
 
   @Test
-  void getScore() {
+  void testThatGetScoreReturnsCorrect() {
     assertEquals(player.getScore(), 30);
   }
 
   @Test
-  void addGold() {
+  void testThatAddGoldReturnsCorrect() {
     player.addGold(10);
     assertEquals(player.getGold(),60);
   }
 
   @Test
-  void getGold() {
+  void testThatGetGoldReturnsCorrect() {
     assertEquals(player.getGold(), 50);
   }
 
-  @Test
-  void addToInventory() {
-    player.addToInventory(test);
-    assertNotNull(player.getInventory());
-  }
-
-  @Test
-  void getInventory() {
-    inventory.add(test);
-    assertFalse(inventory.isEmpty());
-  }
 }
 
