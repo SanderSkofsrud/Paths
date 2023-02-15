72ab17ad0dacc29dbc5626c567e612e70901a65e
sande <sander@skofsrud.net>
2023-02-15 13:27:49 +0100

    Added tests to GoalsTest and ActionsTest

--- src/test/java/edu/ntnu/idatt2001/actions/ActionsTest.java
+++ src/test/java/edu/ntnu/idatt2001/actions/ActionsTest.java
@@ -1,16 +1,23 @@
 package edu.ntnu.idatt2001.actions;
 
+import edu.ntnu.idatt2001.Player;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class ActionsTest {
+
+  Player player = new Player("Test", 10, 30, 50);
 
   @Nested
   class GoldActionTest {
 
     @Test
     void execute() {
-
+      GoldAction goldAction = new GoldAction(10);
+      goldAction.execute(player);
+      assertEquals(60, player.getGold());
     }
   }
 
@@ -18,7 +25,9 @@ public class ActionsTest {
   class HealthActionTest {
     @Test
     void execute (){
-
+      HealthAction healthAction = new HealthAction(10);
+      healthAction.execute(player);
+      assertEquals(20, player.getHealth());
     }
   }
 
@@ -27,6 +36,9 @@ public class ActionsTest {
 
     @Test
     void execute() {
+      InventoryAction inventoryAction = new InventoryAction("Item");
+      inventoryAction.execute(player);
+      assertEquals(1, player.getInventory().size());
     }
   }
 
@@ -34,6 +46,9 @@ public class ActionsTest {
   class ScoreActiontest {
     @Test
     void execute() {
+      ScoreAction scoreAction = new ScoreAction(10);
+      scoreAction.execute(player);
+      assertEquals(40, player.getScore());
 
     }
   }
