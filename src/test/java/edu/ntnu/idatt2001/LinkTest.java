d501484d441c1705a7fea60ffd8857a842910d8c
Helle Rosenlind <helros@hotmail.no>
2023-02-21 13:35:36 +0100

    added nested class and display names to LinkTest

--- src/test/java/edu/ntnu/idatt2001/LinkTest.java
+++ src/test/java/edu/ntnu/idatt2001/LinkTest.java
@@ -3,6 +3,8 @@ package edu.ntnu.idatt2001;
 import edu.ntnu.idatt2001.actions.Action;
 import edu.ntnu.idatt2001.actions.GoldAction;
 import edu.ntnu.idatt2001.actions.HealthAction;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -18,41 +20,57 @@ class LinkTest {
 
   List<Action> actions = new ArrayList<>();
 
-  @Test
-  void testThatConstructorConstructsObject() {
-    assertEquals(Link.class, link.getClass());
+  @Nested
+  public class Constructor {
+    @Test
+    @DisplayName("Test that constructor constructs object")
+    void testThatConstructorConstructsObject() {
+      assertEquals(Link.class, link.getClass());
+    }
+
+    @Test
+    @DisplayName("Test that constructor throws NullPointerException when text is null")
+    void testThatConstructorThrowsNullPointerException() {
+      String text = null;
+      String refrence = "Test";
+      assertThrows(NullPointerException.class, () -> new Link(text, refrence));
+    }
   }
 
-  @Test
-  void testThatConstructorThrowsNullPointerException() {
-    String text = null;
-    String refrence = "Test";
-    assertThrows(NullPointerException.class, () -> new Link(text, refrence));
-  }
-
-  @Test
-  void testThatExceptionIsThrownWhenActionIsNull(){
-    Action action = null;
-    assertThrows(IllegalArgumentException.class, () -> link.addAction(action));
+  @Nested
+  public class ExceptionHandling {
+    @Test
+    @DisplayName("Test that addAction throws IllegalArgumentException when action is null")
+    void testThatExceptionIsThrownWhenActionIsNull() {
+      Action action = null;
+      assertThrows(IllegalArgumentException.class, () -> link.addAction(action));
+    }
   }
 
 
-  @Test
-  void testThatGetTextReturnsCorrect() {
-    assertEquals(link.getText(), "Test");
+  @Nested
+  public class ReturnValues {
+    @Test
+    @DisplayName("Test that getText returns correct")
+    void testThatGetTextReturnsCorrect() {
+      assertEquals(link.getText(), "Test");
+    }
+
+    @Test
+    @DisplayName("Test that getReference returns correct")
+    void testThatGetReferenceReturnsCorrect() {
+      assertEquals(link.getReference(), "reference");
+    }
+
+    @Test
+    @DisplayName("Test that getActions returns correct")
+    void testThatGetActionsReturnsCorrect() {
+      actions.add(goldAction);
+      actions.add(healthAction);
+      link.addAction(goldAction);
+      link.addAction(healthAction);
+      assertEquals(link.getActions(), actions);
+    }
   }
 
-  @Test
-  void testThatGetReferenceReturnsCorrect() {
-    assertEquals(link.getReference(), "reference");
-  }
-
-  @Test
-  void testThatGetActionsReturnsCorrect() {
-    actions.add(goldAction);
-    actions.add(healthAction);
-    link.addAction(goldAction);
-    link.addAction(healthAction);
-    assertEquals(link.getActions(), actions);
-  }
 }
