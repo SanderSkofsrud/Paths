e414cbe9749e67ac88d66370e8a96d7ed738a4ec
Helle Rosenlind <helros@hotmail.no>
2023-02-21 13:52:08 +0100

    added nested class and display names to StoryTest

--- src/test/java/edu/ntnu/idatt2001/StoryTest.java
+++ src/test/java/edu/ntnu/idatt2001/StoryTest.java
@@ -1,47 +1,62 @@
 package edu.ntnu.idatt2001;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class StoryTest {
-  @Test
-  void testThatConstructorThrowsNullPointerExceptions(){
-    Passage passage = null;
-    assertThrows(NullPointerException.class, () ->
-        new Story("Title", passage));
+
+  @Nested
+  public class Constructor {
+    @Test
+    @DisplayName("Test that constructor throws NullPointerExceptions")
+    void testThatConstructorThrowsNullPointerExceptions(){
+      Passage passage = null;
+      assertThrows(NullPointerException.class, () ->
+          new Story("Title", passage));
+    }
   }
 
-  @Test
-  void testThatPassageIsAddedAndFetched(){
-    Passage passage = new Passage("title", "content");
-    Story story = new Story("title", passage);
-    assertEquals(passage, story.getOpeningPassage());
-  }
+  @Nested
+  public class ReturnValues {
 
-  @Test
-  void testThatGetStoryReturnsCorrectly(){
-    Passage openingPassage = new Passage("title", "content");
-    Story story = new Story("title", openingPassage);
+    @Test
+    @DisplayName("Test that passage is added and fetched")
+    void testThatPassageIsAddedAndFetched() {
+      Passage passage = new Passage("title", "content");
+      Story story = new Story("title", passage);
+      assertEquals(passage, story.getOpeningPassage());
+    }
 
-    assertEquals("title", story.getTitle());
-  }
+    @Test
+    @DisplayName("Test that getStory returns correctly")
+    void testThatGetStoryReturnsCorrectly() {
+      Passage openingPassage = new Passage("title", "content");
+      Story story = new Story("title", openingPassage);
 
-  @Test
-  void testThatGetOpeningPassageReturnsCorrectly(){
-    Passage openingPassage = new Passage("title", "content");
-    Story story = new Story("title", openingPassage);
+      assertEquals("title", story.getTitle());
+    }
 
-    assertEquals(openingPassage, story.getOpeningPassage());
-  }
+    @Test
+    @DisplayName("Test that getOpeningPassage returns correctly")
+    void testThatGetOpeningPassageReturnsCorrectly() {
+      Passage openingPassage = new Passage("title", "content");
+      Story story = new Story("title", openingPassage);
 
-  @Test
-  void testThatGetPassagesReturnsCorrectly(){
-    Passage openingPassage = new Passage("title", "content");
-    Story story = new Story("title", openingPassage);
-    Passage passage = new Passage("title2", "content2");
-    story.addPassage(passage);
-    assertEquals(2, story.getPassages().size());
+      assertEquals(openingPassage, story.getOpeningPassage());
+    }
+
+    @Test
+    @DisplayName("Test that getPassages returns correctly")
+    void testThatGetPassagesReturnsCorrectly() {
+      Passage openingPassage = new Passage("title", "content");
+      Story story = new Story("title", openingPassage);
+      Passage passage = new Passage("title2", "content2");
+      story.addPassage(passage);
+      assertEquals(2, story.getPassages().size());
+    }
   }
 }
