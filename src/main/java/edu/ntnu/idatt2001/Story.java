d03156aac89e8c0f65535436223516f85811c428
Helle Rosenlind <helros@hotmail.no>
2023-02-23 15:12:30 +0100

    added Javadoc to Story class

--- src/main/java/edu/ntnu/idatt2001/Story.java
+++ src/main/java/edu/ntnu/idatt2001/Story.java
@@ -4,15 +4,26 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A class that represents a story.
+ */
+
 public class Story {
 
   private String title;
   private Map<Link, Passage> passages;
   private Passage openingPassage;
 
+  /**
+   * Constructor for Story.
+   *
+   * @param title The title of the story.
+   * @param openingPassage The opening passage of the story.
+   * @throws NullPointerException if title or openingPassage is null.
+   */
   public Story(String title, Passage openingPassage) throws NullPointerException {
 
-    if (title == null ||title.isEmpty()) {
+    if (title == null || title.isEmpty()) {
       throw new NullPointerException("Title must not be empty");
     }
     if (openingPassage == null) {
@@ -25,44 +36,89 @@ public class Story {
     addPassage(openingPassage);
   }
 
+  /**
+   * Copy constructor for Story.
+   *
+   * @param story The story to be copied.
+   * @throws NullPointerException if story is null.
+   */
   public Story(Story story) {
     this.title = story.title;
     this.openingPassage = story.openingPassage;
     this.passages = story.passages;
   }
 
+  /**
+   * Returns the title of the story.
+   *
+   * @return The title of the story.
+   */
   public String getTitle() {
     return title;
   }
 
+  /**
+   * Returns the passages of the story.
+   *
+   * @return The passages of the story.
+   */
   public Map<Link, Passage> getPassages() {
     return passages;
   }
+
+  /**
+   * Returns the opening passage of the story.
+   *
+   * @return The opening passage of the story.
+   */
 
   public Passage getOpeningPassage() {
     return openingPassage;
   }
 
-  public void addPassage(Passage passage){
-    if (passage == null){
+  /**
+   * Adds a passage to the story.
+   *
+   * @param passage The passage to be added.
+   * @throws IllegalArgumentException if passage is null.
+   */
+
+  public void addPassage(Passage passage) {
+    if (passage == null) {
       throw new IllegalArgumentException("Passage can not be null");
     }
     passages.put(new Link(passage.getTitle(), passage.getTitle()), passage);
   }
 
+  /**
+   * Returns the passage that the link leads to.
+   *
+   * @param link The link to the passage.
+   * @return Passage
+   * @throws NullPointerException if link is null.
+   */
+
   public Passage getPassage(Link link) throws NullPointerException {
-    if (link == null){
+    if (link == null) {
       throw new NullPointerException("Story-Link cannot be null");
     }
     return passages.get(link);
   }
 
+  /**
+   * Return a String representation of the story.
+   *
+   * @return A String representation of the story.
+   */
   @Override
   public String toString() {
-    return "Story{" +
-        "title='" + getTitle() + '\'' +
-        ", passages=" + getPassages() +
-        ", openingPassage=" + getOpeningPassage() +
-        '}';
+    return "Story{"
+        + "title='"
+        + getTitle() + '\''
+        + ", passages="
+        + getPassages()
+        + ", openingPassage="
+        + getOpeningPassage()
+        + '}';
   }
 }
