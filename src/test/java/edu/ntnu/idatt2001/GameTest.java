ef440f48fc2bd6bda00ef49be99f67ae88e8d3fd
Helle Rosenlind <helros@hotmail.no>
2023-02-23 13:31:16 +0100

    added a few more tests and beforeEach

--- src/test/java/edu/ntnu/idatt2001/GameTest.java
+++ src/test/java/edu/ntnu/idatt2001/GameTest.java
@@ -2,9 +2,7 @@ package edu.ntnu.idatt2001;
 
 import edu.ntnu.idatt2001.goals.Goal;
 import edu.ntnu.idatt2001.goals.ScoreGoal;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +10,37 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
+  String name = "";
+  int score;
+  int health;
+  int gold;
+  int minimumScore;
+  String title = "";
+  String content = "";
 
+  @BeforeEach
+  void setUp() {
+    name = "name";
+    score = 50;
+    health = 10;
+    gold = 30;
+    minimumScore = 20;
+    title = "title";
+    content = "content";
+  }
+
+  @Nested
+  public class constructor {
+    @Test
+    @DisplayName("Test that constructor constructs object")
+    void testThatConstructorConstructsObject() {
+      Player player = new Player(name, health, score, gold);
+      Story story = new Story(title, new Passage(title, content));
+      List<Goal> goals = new ArrayList<>();
+      Game game = new Game(player, story, goals);
+      assertEquals(Game.class, game.getClass());
+    }
+  }
   @Nested
   public class ExceptionHandling {
     @Test
@@ -26,15 +54,36 @@ public class GameTest {
     @Test
     @DisplayName("Test that constructor throws NullPointerException when story is null")
     void testThatBeginReturnsOpeningPassage(){
-      Player player = new Player("name", 50, 10, 30);
-      Passage openingPassage = new Passage("title", "content");
-      Story story = new Story("title", openingPassage);
+      Player player = new Player(name, health, score, gold);
+      Passage openingPassage = new Passage(title, content);
+      Story story = new Story(title, openingPassage);
       List<Goal> goals = new ArrayList<>();
 
       Game game = new Game(player, story, goals);
-      goals.add(new ScoreGoal(20));
+      goals.add(new ScoreGoal(minimumScore));
 
       assertEquals(openingPassage, game.begin());
+    }
+    @Test
+    @DisplayName("Test that constructor throws NullPointerException when goals is null")
+    void testThatConstructorThrowsNullPointerExceptionWhenGoalsIsNull() {
+      Player player = new Player(name, health, score, gold);
+      Story story = new Story(title, new Passage(title, content));
+      List<Goal> goals = null;
+      assertThrows(NullPointerException.class, () -> new Game(player,story,goals));
+    }
+
+    @Test
+    @DisplayName("Test that go throws NullPointerException when link is null")
+    void testThatGoThrowsNullPointerException() {
+      Player player = new Player(name, health, score, gold);
+      Passage openingPassage = new Passage(title, content);
+      Story story = new Story(title, openingPassage);
+      List<Goal> goals = new ArrayList<>();
+      goals.add(new ScoreGoal(minimumScore));
+      Game game = new Game(player, story, goals);
+
+      assertThrows(NullPointerException.class, () -> game.go(null));
     }
   }
 
@@ -44,12 +93,12 @@ public class GameTest {
    @Test
    @DisplayName("Test that go returns passage")
    void testThatGoReturnsPassage() {
-     Player player = new Player("name", 50, 10, 30);
-     Passage openingPassage = new Passage("title", "content");
-     Story story = new Story("title", openingPassage);
+     Player player = new Player(name, health, score, gold);
+     Passage openingPassage = new Passage(title, content);
+     Story story = new Story(title, openingPassage);
      List<Goal> goals = new ArrayList<>();
      Passage passage = new Passage("title2", "content2");
-     goals.add(new ScoreGoal(20));
+     goals.add(new ScoreGoal(minimumScore));
      Game game = new Game(player, story, goals);
      game.getStory().addPassage(passage);
 
@@ -57,13 +106,26 @@ public class GameTest {
    }
 
    @Test
+   @DisplayName("Test that begin returns opening passage")
+    void testThatBeginReturnsOpeningPassage() {
+      Player player = new Player(name, health, score, gold);
+      Passage openingPassage = new Passage(title, content);
+      Story story = new Story(title, openingPassage);
+      List<Goal> goals = new ArrayList<>();
+      goals.add(new ScoreGoal(minimumScore));
+      Game game = new Game(player, story, goals);
+
+      assertEquals(openingPassage, game.begin());
+    }
+
+   @Test
    @DisplayName("Test that getStory returns correct story")
    void testThatGetStoryReturnsCorrect() {
-     Player player = new Player("name", 50, 10, 30);
-     Passage openingPassage = new Passage("title", "content");
-     Story story = new Story("title", openingPassage);
+     Player player = new Player(name, health, score, gold);
+     Passage openingPassage = new Passage(title, content);
+     Story story = new Story(title, openingPassage);
      List<Goal> goals = new ArrayList<>();
-     goals.add(new ScoreGoal(20));
+     goals.add(new ScoreGoal(minimumScore));
      Game game = new Game(player, story, goals);
 
      assertEquals(story, game.getStory());
@@ -72,11 +134,11 @@ public class GameTest {
    @Test
    @DisplayName("Test that getGoals returns correct goals")
    void testThatGetGoalsReturnsCorrect() {
-     Player player = new Player("name", 50, 10, 30);
-     Passage openingPassage = new Passage("title", "content");
-     Story story = new Story("title", openingPassage);
+     Player player = new Player(name, health, score, gold);
+     Passage openingPassage = new Passage(title, content);
+     Story story = new Story(title, openingPassage);
      List<Goal> goals = new ArrayList<>();
-     goals.add(new ScoreGoal(20));
+     goals.add(new ScoreGoal(minimumScore));
      Game game = new Game(player, story, goals);
 
      assertEquals(goals, game.getGoals());
@@ -85,11 +147,11 @@ public class GameTest {
    @Test
    @DisplayName("Test that getPlayers returns correct player")
    void testThatGetPlayersReturnsCorrect() {
-     Player player = new Player("name", 50, 10, 30);
-     Passage openingPassage = new Passage("title", "content");
-     Story story = new Story("title", openingPassage);
+     Player player = new Player(name, health, score, gold);
+     Passage openingPassage = new Passage(title, content);
+     Story story = new Story(title, openingPassage);
      List<Goal> goals = new ArrayList<>();
-     goals.add(new ScoreGoal(20));
+     goals.add(new ScoreGoal(minimumScore));
      Game game = new Game(player, story, goals);
 
      assertEquals(player, game.getPlayer());
