ccc7621e5a296d0ecf3684873ddff7ac54b0103e
Helle Rosenlind <helros@hotmail.no>
2023-04-11 14:41:01 +0200

    changed execute method after changing addHealth method in Player and added javadoc

--- src/main/java/edu/ntnu/idatt2001/paths/backend/actions/HealthAction.java
+++ src/main/java/edu/ntnu/idatt2001/paths/backend/actions/HealthAction.java
@@ -3,34 +3,41 @@ package edu.ntnu.idatt2001.paths.backend.actions;
 import edu.ntnu.idatt2001.paths.backend.Player;
 
 /**
- * The interface Action.
+ * Health action.
+ * An action that adds or removes health to the player when executed.
+ *
+ * @author Helle R. & Sander S.
+ * @version 0.1 - 11.04.2023
  */
 public class HealthAction implements Action {
 
   private final int health;
 
   /**
-   * Instantiates a new Health action.
+   * Instantiates a new Health action which adds or removes health to the player.
    *
-   * @param health the health
+   * @param health the amount of health to add or remove
    */
   public HealthAction(int health) {
     this.health = health;
   }
 
   /**
-   * Executes a health action.
+   * Executes a gold action on a player.
+   * Uses the addHealth method in the Player class.
    *
    * @param player the player
    */
   @Override
   public void execute(Player player) {
-    if (player.getHealth() + this.health < 0) {
-      throw new IllegalArgumentException("Health cannot be less than 0");
-    }
     player.addHealth(health);
   }
 
+  /**
+   * Returns a string representation of the HealthAction object.
+   *
+   * @return a string representation of the HealthAction object.
+   */
   @Override
   public String toString() {
     return "{HealthAction}(" + this.health + ")";
