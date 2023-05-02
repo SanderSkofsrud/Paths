6db979a153b7fe4c01978b35ba20148cfb0e80a6
sande <sander@skofsrud.net>
2023-05-02 10:26:48 +0200

    Added new GameData class to store all data about a game but that still can be null

---
+++ src/main/java/edu/ntnu/idatt2001/paths/utility/GameData.java
@@ -0,0 +1,32 @@
+package edu.ntnu.idatt2001.paths.utility;
+
+import edu.ntnu.idatt2001.paths.models.Player;
+import edu.ntnu.idatt2001.paths.models.Story;
+import edu.ntnu.idatt2001.paths.models.goals.Goal;
+
+import java.util.List;
+
+public class GameData {
+  private Story story;
+  private Player player;
+  private List<Goal> goals;
+
+  public GameData(Story story, Player player, List<Goal> goals) {
+    this.story = story;
+    this.player = player;
+    this.goals = goals;
+  }
+
+  public Story getStory() {
+    return story;
+  }
+
+  public Player getPlayer() {
+    return player;
+  }
+
+  public List<Goal> getGoals() {
+    return goals;
+  }
+}
+
