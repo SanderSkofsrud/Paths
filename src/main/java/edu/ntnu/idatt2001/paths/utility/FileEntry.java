d02111f79979c2c127f3c7b52acf86bdeb704d1e
sande <sander@skofsrud.net>
2023-05-03 09:57:58 +0200

    Changed FileEntry to include the location of the file, as this is needed to be shown

--- src/main/java/edu/ntnu/idatt2001/paths/utility/FileEntry.java
+++ src/main/java/edu/ntnu/idatt2001/paths/utility/FileEntry.java
@@ -1,12 +1,17 @@
 package edu.ntnu.idatt2001.paths.utility;
 
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.ObservableList;
+import edu.ntnu.idatt2001.paths.models.Link;
 
 public class FileEntry {
   private final SimpleStringProperty fileName;
+  private final SimpleStringProperty fileLocation;
 
-  public FileEntry(String fileName) {
+  public FileEntry(String fileName, String fileLocation) {
     this.fileName = new SimpleStringProperty(fileName);
+    this.fileLocation = new SimpleStringProperty(fileLocation);
   }
 
   public String getFileName() {
@@ -15,5 +20,13 @@ public class FileEntry {
 
   public void setFileName(String fileName) {
     this.fileName.set(fileName);
+  }
+
+  public String getFileLocation() {
+    return fileLocation.get();
+  }
+
+  public void setFileLocation(String fileLocation) {
+    this.fileLocation.set(fileLocation);
   }
 }
