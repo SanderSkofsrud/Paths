a5b2a785e54e8e28b7b1cc3708a88f3a5aaacbc5
sande <sander@skofsrud.net>
2023-05-05 15:21:01 +0200

    Added actions to PathsReader

--- src/main/java/edu/ntnu/idatt2001/paths/utility/paths/PathsReader.java
+++ src/main/java/edu/ntnu/idatt2001/paths/utility/paths/PathsReader.java
@@ -1,7 +1,10 @@
 package edu.ntnu.idatt2001.paths.utility.paths;
 
 import edu.ntnu.idatt2001.paths.models.*;
+import edu.ntnu.idatt2001.paths.models.actions.*;
 import edu.ntnu.idatt2001.paths.models.goals.*;
+import edu.ntnu.idatt2001.paths.models.player.Player;
+import edu.ntnu.idatt2001.paths.models.player.PlayerBuilder;
 import edu.ntnu.idatt2001.paths.utility.GameData;
 
 import java.io.File;
@@ -12,6 +15,8 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static edu.ntnu.idatt2001.paths.models.goals.GoalEnum.*;
 
 public class PathsReader {
   static List<String> inventoryGoals = new ArrayList<>();
@@ -91,20 +96,27 @@ public class PathsReader {
     Pattern linkPattern = Pattern.compile("\\[(.*?)\\]\\((.*?)\\)");
     Pattern playerPattern = Pattern.compile("^;;(.+);(\\d+);(\\d+);(\\d+);\\[(.*)\\]");
     Pattern goalPattern = Pattern.compile("^\\((.+Goal)\\)\\((.+)\\)");
+    Pattern actionPattern = Pattern.compile("\\{(.*?)\\}\\((.*?)\\)");
 
     List<Goal> goals = new ArrayList<>();
 
     while (scanner.hasNextLine()) {
       String line = scanner.nextLine();
 
+      if (line.isBlank()) {
+        continue;
+      }
+
       Matcher passageMatcher = passagePattern.matcher(line);
       Matcher linkMatcher = linkPattern.matcher(line);
       Matcher playerMatcher = playerPattern.matcher(line);
       Matcher goalMatcher = goalPattern.matcher(line);
+      Matcher actionMatcher = actionPattern.matcher(line);
 
       if (passageMatcher.find()) {
         String passageTitle = passageMatcher.group(2);
-        String content = scanner.nextLine();
+        String content;
+        content = scanner.nextLine();
         Passage newPassage = new Passage(passageTitle, content);
 
         if (openingPassage == null) {
@@ -119,6 +131,23 @@ public class PathsReader {
         String linkDescription = linkMatcher.group(1);
         String linkReference = linkMatcher.group(2);
         Link link = new Link(linkDescription, linkReference);
+
+        List<Action> actions = new ArrayList<>();
+        while (actionMatcher.find()) {
+          String actionType = actionMatcher.group(1);
+          String actionValue = actionMatcher.group(2);
+          switch (actionType) {
+            case "GoldAction" -> actions.add(new GoldAction(Integer.parseInt(actionValue)));
+            case "ScoreAction" -> actions.add(new ScoreAction(Integer.parseInt(actionValue)));
+            case "HealthAction" -> actions.add(new HealthAction(Integer.parseInt(actionValue)));
+            case "InventoryAction" -> actions.add(new InventoryAction(actionValue));
+            default -> throw new IllegalArgumentException("Invalid action type: " + actionType);
+          }
+        }
+        for (Action action : actions) {
+          link.addAction(action);
+        }
+
         currentPassage.addLink(link);
       } else if (playerMatcher.find()) {
         String playerName = playerMatcher.group(1);
@@ -126,7 +155,7 @@ public class PathsReader {
         int playerScore = Integer.parseInt(playerMatcher.group(3));
         int playerGold = Integer.parseInt(playerMatcher.group(4));
         List<String> playerInventory = Arrays.asList(playerMatcher.group(5).split(","));
-        player = new Player.Builder(playerName)
+        player = new PlayerBuilder(playerName)
                 .health(playerHealth)
                 .score(playerScore)
                 .gold(playerGold)
@@ -137,35 +166,27 @@ public class PathsReader {
         String goalDescription = goalMatcher.group(2);
         Goal goal = createGoal(goalType, goalDescription);
         goals.add(goal);
-      } else if (line.isBlank()) {
-        continue;
-      } else {
-        throw new IllegalArgumentException("The file is not in the correct format");
-      }
-    }
-    scanner.close();
-    if (!inventoryGoals.isEmpty()) {
-      goals.add(new InventoryGoal(inventoryGoals));
-    }
-
-    if (story == null) {
+    } else {
       throw new IllegalArgumentException("The file is not in the correct format");
     }
-    return new GameData(story, player, goals);
   }
+scanner.close();
+if (!inventoryGoals.isEmpty()) {
+    goals.add(GoalFactory.createInventoryGoal(String.valueOf(inventoryGoals)));
+  }
+
+if (story == null) {
+    throw new IllegalArgumentException(fileName + "The file is not in the correct format");
+  }
+return new GameData(story, player, goals);
+}
 
 
   private static Goal createGoal(String goalType, String goalDescription) {
     switch (goalType) {
-      case "HealthGoal" -> {
-        return new HealthGoal(Integer.parseInt(goalDescription));
-      }
-      case "ScoreGoal" -> {
-        return new ScoreGoal(Integer.parseInt(goalDescription));
-      }
-      case "GoldGoal" -> {
-        return new GoldGoal(Integer.parseInt(goalDescription));
-      }
+      case "HealthGoal" -> GoalFactory.createGoal(HEALTH, Integer.parseInt(goalDescription));
+      case "ScoreGoal" -> GoalFactory.createGoal(SCORE, Integer.parseInt(goalDescription));
+      case "GoldGoal" -> GoalFactory.createGoal(GOLD, Integer.parseInt(goalDescription));
       case "InventoryGoal" -> inventoryGoals.add(goalDescription);
       default -> throw new IllegalArgumentException("The goal type is not valid");
     }
