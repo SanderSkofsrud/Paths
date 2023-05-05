0d1dc248ee0012c44a8050b363f5c6c7430b2213
sande <sander@skofsrud.net>
2023-05-05 15:21:28 +0200

    Updated module-info.java to include new changes made

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,8 +9,13 @@ module paths {
     exports edu.ntnu.idatt2001.paths.views;
     exports edu.ntnu.idatt2001.paths.controllers;
     exports edu.ntnu.idatt2001.paths.utility;
+    exports edu.ntnu.idatt2001.paths.models.goals;
+    exports edu.ntnu.idatt2001.paths.models.actions;
 
     opens edu.ntnu.idatt2001.paths.models to com.google.gson;
     opens edu.ntnu.idatt2001.paths.models.goals to com.google.gson;
+    opens edu.ntnu.idatt2001.paths.models.actions to com.google.gson;
     exports edu.ntnu.idatt2001.paths.utility.json;
+    exports edu.ntnu.idatt2001.paths.models.player;
+    opens edu.ntnu.idatt2001.paths.models.player to com.google.gson;
 }
