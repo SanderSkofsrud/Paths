ae1da7c58366a8575158ecff97a8f1d29054abb1
sande <sander@skofsrud.net>
2023-05-20 15:16:25 +0200

    Moved logic out of LoadGameView and into LoadGameController

--- src/main/java/edu/ntnu/idatt2001/paths/views/LoadGameView.java
+++ src/main/java/edu/ntnu/idatt2001/paths/views/LoadGameView.java
@@ -98,104 +98,7 @@ public class LoadGameView extends View{
    */
   @Override
   public void setup() {
-    jsonTableView = new TableView<>();
-
-    TableColumn<File, String> fileNameColumn = new TableColumn<>(languageController.getTranslation(Dictionary.FILE_NAME.getKey()));
-    fileNameColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getName()));
-
-    TableColumn<File, String> fileLocationColumn = new TableColumn<>(languageController.getTranslation(Dictionary.FILE_LOCATION.getKey()));
-    fileLocationColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getAbsolutePath()));
-    fileLocationColumn.setCellFactory(tc -> new TableCell<>() {
-      private final Text text = new Text();
-      private final VBox vbox = new VBox(text);
-      private final Pane container = new Pane(vbox);
-      private final TranslateTransition tt = new TranslateTransition(Duration.seconds(6.5), text);
-      private final PauseTransition ptBefore = new PauseTransition(Duration.seconds(1));
-      private final PauseTransition ptAfter = new PauseTransition(Duration.seconds(1));
-      private final SequentialTransition st = new SequentialTransition(ptBefore, tt, ptAfter);
-
-      {
-        st.setCycleCount(Timeline.INDEFINITE);
-
-        text.layoutBoundsProperty().addListener((observable, oldValue, newValue) -> {
-          container.setMinWidth(newValue.getWidth());
-          tt.setFromX(container.getWidth());
-          tt.setToX(-newValue.getWidth() + 365);
-        });
-        container.setPrefWidth(100);  // adjust to desired width
-        vbox.setPrefWidth(100);  // adjust to desired width
-        vbox.setFillWidth(true);
-        vbox.setAlignment(Pos.CENTER);
-      }
-
-      @Override
-      protected void updateItem(String item, boolean empty) {
-        super.updateItem(item, empty);
-        if (empty) {
-          setGraphic(null);
-          text.setText(null);
-          st.stop();
-        } else {
-          text.setText(item);
-          setGraphic(container);
-          st.play();
-        }
-      }
-
-      @Override
-      public void startEdit() {
-        super.startEdit();
-        st.pause();
-      }
-
-      @Override
-      public void cancelEdit() {
-        super.cancelEdit();
-        st.play();
-      }
-    });
-
-
-    fileLocationColumn.setPrefWidth(370);
-
-    TableColumn<File, String> brokenLinksColumn = new TableColumn<>(languageController.getTranslation(Dictionary.BROKEN_LINKS.getKey()));
-    brokenLinksColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(loadGameController.getBrokenLinksString(param.getValue())));
-
-    TableColumn<File, String> loadColumn = new TableColumn<>(languageController.getTranslation(Dictionary.LOAD_GAME.getKey()));
-    loadColumn.setPrefWidth(100);
-    loadColumn.setCellFactory(tc -> new TableCell<>() {
-      private final Hyperlink hyperlink = new Hyperlink(languageController.getTranslation(Dictionary.LOAD_GAME.getKey()));
-
-      {
-        hyperlink.setOnAction(event -> {
-          String selectedFile = getTableView().getItems().get(getIndex()).getName();
-          System.out.println("Clicked on file: " + selectedFile);
-          try {
-            game = fileHandlerController.loadGameJson(selectedFile);
-            gameController.setGame(game);
-            playerController.setActiveCharacter(game.getPlayer().getCharacterModel());
-          } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-          }
-          screenController.activate("MainGame");
-          resetPane();
-        });
-      }
-
-      @Override
-      protected void updateItem(String item, boolean empty) {
-        super.updateItem(item, empty);
-        if (empty) {
-          setGraphic(null);
-        } else {
-          setGraphic(hyperlink);
-        }
-      }
-    });
-
-    jsonTableView.getColumns().addAll(fileNameColumn, fileLocationColumn, brokenLinksColumn, loadColumn);
-    jsonTableView.setItems(loadGameController.getSavedGames("json"));
-    jsonTableView.setPrefWidth(650);
+    jsonTableView = loadGameController.createTableView(screenController);
 
     HBox hBox = new HBox();
     hBox.getChildren().add(jsonTableView);
@@ -215,28 +118,7 @@ public class LoadGameView extends View{
     Button uploadButton = new Button(languageController.getTranslation(Dictionary.UPLOAD_FILE.getKey()));
 
     uploadButton.setOnAction(event -> {
-      FileChooser fileChooser = new FileChooser();
-      fileChooser.setTitle("Upload Game File");
-      fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
-
-      fileChooser.getExtensionFilters().addAll(
-              new FileChooser.ExtensionFilter("PATHS Files", "*.paths")
-      );
-
-      File selectedFile = fileChooser.showOpenDialog(null);
-
-      if (selectedFile != null) {
-        System.out.println("Selected file: " + selectedFile.getName());
-        String extension = selectedFile.getName().substring(selectedFile.getName().lastIndexOf(".") + 1);
-
-        if (extension.equals("paths")) {
-          try {
-            loadGameController.addSavedGame(selectedFile.getName(), "paths", selectedFile);
-          } catch (IOException e) {
-            throw new RuntimeException(e);
-          }
-        }
-      }
+      loadGameController.uploadGameFile();
     });
 
     VBox vBox = new VBox();
