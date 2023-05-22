7c0828fc4f33dd8255bfc3d451b67726bcf9097b
sande <sander@skofsrud.net>
2023-05-22 14:23:14 +0200

    Updated javadoc

--- src/main/java/edu/ntnu/idatt2001/paths/controllers/LanguageController.java
+++ src/main/java/edu/ntnu/idatt2001/paths/controllers/LanguageController.java
@@ -9,7 +9,6 @@ import edu.ntnu.idatt2001.paths.utility.Language;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -17,13 +16,42 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+/**
+ * The type Language controller.
+ * Singleton class for the language controller
+ * Can be accessed from anywhere in the program.
+ * This class is responsible for handling the language of the game.
+ *
+ * @author Helle R. and Sander S.
+ * @version 1.1 15.05.2023
+ */
 public class LanguageController {
 
+  /**
+   * The constant instance.
+   */
   public static LanguageController instance;
-  private Language currentLanguage = Language.EN;  // Default language
+  /**
+   * The Current language.
+   * English is default
+   */
+  private Language currentLanguage = Language.EN;
+  /**
+   * The Google translate API
+   */
   private Translate translate;
+  /**
+   * The Dictionary.
+   * Contains all the words in the game
+   * The key is the English word, and the value is the translated word
+   */
   private Map<String, String> translations;
 
+  /**
+   * The constructor of the class.
+   * Sets up the google translate API
+   * Loads the config.properties file and uses the google.cloud.api.key to set up the API
+   */
   private LanguageController() {
     translations = new HashMap<>();
     loadTranslations();
@@ -48,6 +76,11 @@ public class LanguageController {
   }
 
 
+  /**
+   * Returns the instance of the class.
+   *
+   * @return the instance of the class
+   */
   public static LanguageController getInstance() {
     if (instance == null) {
       instance = new LanguageController();
@@ -55,18 +88,34 @@ public class LanguageController {
     return instance;
   }
 
-  public String getCurrentLanguage() {
-    return currentLanguage.toString().toUpperCase();
-  }
+  /**
+   * Gets current language enum.
+   *
+   * @return the current language enum
+   */
   public Language getCurrentLanguageEnum() {
     return currentLanguage;
   }
 
+  /**
+   * Sets language.
+   * Sets the current language
+   * Loads the translations for the new language
+   *
+   * @param language the language
+   */
   public void setLanguage(String language) {
     this.currentLanguage = Language.valueOf(language.toUpperCase());
     loadTranslations();
   }
 
+  /**
+   * Is internet available boolean.
+   * Checks if the internet is available
+   * Used to check if the google translate API can be used
+   *
+   * @return returns true if the internet is available, false if not
+   */
   public boolean isInternetAvailable() {
     try {
       final URL url = new URL("http://www.google.com");
@@ -82,6 +131,14 @@ public class LanguageController {
   }
 
 
+  /**
+   * Translate string.
+   * Translates the source text to the current language
+   * If the internet is not available or the target language is the same as the source language, return the source text
+   *
+   * @param sourceText the text to be translated
+   * @return the translated text
+   */
   public String translate(String sourceText) {
     String language = this.currentLanguage.toString().toUpperCase();
 
@@ -97,6 +154,13 @@ public class LanguageController {
     return translation.getTranslatedText();
   }
 
+  /**
+   * Translate to english string.
+   * Translates the source text to english
+   *
+   * @param sourceText the text to be translated
+   * @return the translated text
+   */
   public String translateToEnglish(String sourceText) {
     String language = this.currentLanguage.toString().toUpperCase();
 
@@ -112,6 +176,11 @@ public class LanguageController {
     return translation.getTranslatedText();
   }
 
+  /**
+   * Load translations.
+   * Loads the translations for the current language
+   * Stores the translations in the dictionary
+   */
   private void loadTranslations() {
     translations.clear();
 
@@ -123,6 +192,14 @@ public class LanguageController {
     }
   }
 
+  /**
+   * Translate to language string.
+   * Translates the source text to the target language
+   *
+   * @param sourceText the text to be translated
+   * @param language   the target language
+   * @return the translated text
+   */
   private String translateToLanguage(String sourceText, Language language) {
     String languageCode = language.toString().toLowerCase();
     if (languageCode.equals("en")) {
@@ -136,6 +213,13 @@ public class LanguageController {
     return translation.getTranslatedText();
   }
 
+  /**
+   * Gets translation.
+   * Returns the translation for the source text
+   *
+   * @param sourceText the text to get the translation for
+   * @return the translation for the source text
+   */
   public String getTranslation(String sourceText) {
     return translations.getOrDefault(sourceText, sourceText);
   }
